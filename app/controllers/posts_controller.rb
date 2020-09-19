4d70f9b160937349fa2d0d057ec31285df8afd4f
MaSungHo <tonem123@naver.com>
2020-09-19 21:05:26 +0900

    index error

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,21 @@
 class PostsController < ApplicationController
+  before_action load_post only: %i(show edit)
+
   def index
     @posts = Post.all
     render json: @posts, status: :ok
   end
 
+  def show
+    render json: @post, status: :ok
+  end
+
   def new
     @post = Post.new
+    render json: @post, status: :ok
+  end
+  
+  def edit
     render json: @post, status: :ok
   end
 
@@ -14,8 +24,14 @@ class PostsController < ApplicationController
     render json: @post, status: :ok
   end
 
+
   private
   def post_params
     params.require(:post).permit(Post::POST_COLUMNS)
+  end
+
+  private
+  def load_post
+    @post = Post.find(params(:id))
   end
 end
