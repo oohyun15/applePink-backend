6716a576b032fb390e6776c4460c4e18517f8014
MaSungHo <tonem123@naver.com>
2020-09-26 15:41:15 +0900

    Add jwt base

--- Gemfile
+++ Gemfile
@@ -38,6 +38,10 @@ gem "turbolinks", "~> 5"
 gem "jbuilder", "~> 2.7"
 gem "bootsnap", ">= 1.4.2", require: false
 
+# JWT 인증
+gem "jwt"
+gem "figaro"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'api/test'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   root "posts#index"
+
+  post 'auth_user' => 'authentication#authenticate_user'
+  get 'apis/test'
 
   resources :users
   resources :posts
