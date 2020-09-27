7230f09aa45c8952f130b10f9525846fb8b7d532
oohyun15 <sakiss@ajou.ac.kr>
2020-09-27 16:22:46 +0900

    Fix minor bug
    - 함수명 수정

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,6 +1,6 @@
 class ApisController < ApplicationController
   ## API Controller 액션에 방문 전, application Controller의 jwt_authenticate_request! 메소드 발동
-  before_action :jwt_authenticate_request!
+  before_action :authenticate_user!
 
   ## test 액션에 방문에 있어 토큰 검증 성공 시, Token 인증 성공 메세지가 응답될 것입니다.
   def test
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,14 @@
 Rails.application.routes.draw do
 
-  root "posts#index"
-
-  post "auth_user" => "authentication#authenticate_user"
-  get "apis/test"
+  root "authentication#new"
 
   get 'users/sign_in', to: 'authentication#new'
   post 'users/sign_in', to: 'authentication#create'
   get 'users/sign_up', to: 'users#new'
   post 'users/sign_up', to: 'users#create'
-
+  
   resources :users, only: %i(index show edit update destroy)
- 
   resources :posts
   resources :chats
+
 end
