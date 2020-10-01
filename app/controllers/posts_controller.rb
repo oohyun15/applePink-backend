a4a007aec6eae2415f21275e929f49da9a3f6cd0
oohyun15 <sakiss@ajou.ac.kr>
2020-10-01 13:09:36 +0900

    Add validation and user_id

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -19,7 +19,7 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.create! post_params
+    @post = current_user.posts.create! post_params
     redirect_to post_path(@post)
   end
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,10 +1,14 @@
 class Post < ApplicationRecord
   POST_COLUMNS = %i(title body price category_id)  
 
+  validates :title, presence: :true
+  validates :body, presence: :true
+  validates :price, presence: :true
+
   has_many :chats
   has_many :chat_users, through: :chats, source: :users
 
-  belongs_to :user, optional: true
+  belongs_to :user
   belongs_to :category, optional: true
 
   
