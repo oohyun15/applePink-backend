308eb27739973b73d258c073eedf7a46dc4536ca
MaSungHo <tonem123@naver.com>
2020-10-03 23:15:56 +0900

    Add text body to message

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_111814) do
+ActiveRecord::Schema.define(version: 2020_10_03_121450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_09_21_111814) do
   create_table "messages", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "body"
   end
 
   create_table "posts", force: :cascade do |t|
