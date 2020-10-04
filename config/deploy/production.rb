20947f01aba89c8e467608f13dbd48bc50f33beb
oohyun15 <sakiss@ajou.ac.kr>
2020-10-04 14:49:33 +0900

    fix minor bug

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -18,4 +18,4 @@ server fetch(:aws_ec2_ip), port: 22, roles: [:web, :app, :db], primary: true
 
 ## SSH Remote 설정 (서버 아이디 및 pem Key 경로 설정)
 # set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(/home/ubuntu/.ssh/id_rsa) }
-set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(/Downloads/key_applepink.pem) }
+set :ssh_options, { forward_agent: true, user: fetch(:user), keys: %w(/home/ubuntu/.ssh/id_rsa) }
