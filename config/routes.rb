929f08fad6d9b338c1cb4f5ba025d59b2e1efcef
oohyun15 <sakiss@ajou.ac.kr>
2020-10-08 21:38:09 +0900

    Add devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,19 @@
 Rails.application.routes.draw do
 
+  devise_for :users, controllers: {
+    omniauth_callbacks: 'users/omniauth_callbacks'
+  }
   root "authentication#new"
 
   get 'users/sign_in', to: 'authentication#new'
   post 'users/sign_in', to: 'authentication#create'
   post 'users/sign_up', to: 'users#create'
   
-  resources :users, only: %i(index show edit update destroy)
+  resources :users, only: %i(index show edit update destroy) do
+    collection do
+      get :kakao
+    end
+  end
   resources :posts
   resources :chats
 
