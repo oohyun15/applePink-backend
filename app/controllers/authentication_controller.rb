d09f221c79c83c5c86178622b3e0fd3a8586f30f
MaSungHo <tonem123@naver.com>
2020-10-10 15:23:46 +0900

    move payload method

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,5 +1,4 @@
 class AuthenticationController < ApplicationController
-  skip_before_action :verify_authenticity_token
   
   # 로그인 페이지
   def new
@@ -18,6 +17,15 @@ class AuthenticationController < ApplicationController
     else
       render json: { error: "unauthorized" }, status: :unauthorized
     end
+  end
+
+  ## Response으로서 보여줄 json 내용 생성 및 JWT Token 생성
+  def payload(user)
+    ## 해당 코드 예제에서 토큰 만료기간은 '30일' 로 설정
+    @token = JWT.encode({ user_id: user.id, exp: 7.days.from_now.to_i }, ENV["SECRET_KEY_BASE"])
+    # @tree = { jwt: @token, userInfo: { id: user.id, email: user.email } }
+
+    return @token
   end
 
   private
