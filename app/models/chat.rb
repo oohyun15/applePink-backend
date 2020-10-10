733a57b83a5d7df33f4ba80ffe8b8ceae0b87051
oohyun15 <sakiss@ajou.ac.kr>
2020-10-11 00:33:16 +0900

    Fix minor bug
    - chat, message 모델관계 수정

--- app/models/chat.rb
+++ app/models/chat.rb
@@ -2,7 +2,9 @@ class Chat < ApplicationRecord
 
   has_many :user_chats
   has_many :users, through: :user_chats
-  has_many :messages
+  has_many :messages, dependent: :destroy
+
+  accepts_nested_attributes_for :messages, allow_destroy: true
   
   belongs_to :post
 end
