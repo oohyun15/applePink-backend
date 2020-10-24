b0d674713ef1da4bd0c8fb284aa36cc38c0b5629
MaSungHo <tonem123@naver.com>
2020-10-24 16:48:55 +0900

    Edit location foreign key reference

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -4,12 +4,12 @@ class UserSerializer < ActiveModel::Serializer
 
   def user_simple_info
     user_simple_scope = ActiveModel::Type::Boolean.new.cast(scope.dig(:params, :user_simple_info))
-    {id: object.id, title: object.email, nickname: object.nickname, location_id: object.location_id}
+    {id: object.id, title: object.email, nickname: object.nickname, location_title: object.location.title}
   end
 
   def user_detail_info
     user_detail_scope = ActiveModel::Type::Boolean.new.cast(scope.dig(:params, :user_detail_info))
-    {id: object.id, title: object.email, nickname: object.nickname, location_id: object.location_id, user_type: object.user_type} if user_detail_scope rescue nil
+    {id: object.id, title: object.email, nickname: object.nickname, location_title: object.location.title, user_type: object.user_type} if user_detail_scope rescue nil
   end
 
 end
