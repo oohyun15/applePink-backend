5826bad1fb365758830468be244ced434e63907d
oohyun15 <sakiss@ajou.ac.kr>
2020-10-25 13:49:47 +0900

    Add delayed job

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_22_123149) do
+ActiveRecord::Schema.define(version: 2020_10_25_044904) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -70,6 +70,21 @@ ActiveRecord::Schema.define(version: 2020_10_22_123149) do
     t.bigint "post_id"
     t.boolean "has_message", default: false
     t.index ["post_id"], name: "index_chats_on_post_id"
+  end
+
+  create_table "delayed_jobs", force: :cascade do |t|
+    t.integer "priority", default: 0, null: false
+    t.integer "attempts", default: 0, null: false
+    t.text "handler", null: false
+    t.text "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.string "locked_by"
+    t.string "queue"
+    t.datetime "created_at", precision: 6
+    t.datetime "updated_at", precision: 6
+    t.index ["priority", "run_at"], name: "delayed_jobs_priority"
   end
 
   create_table "email_certifications", force: :cascade do |t|
