b8144137029b2e0523d5b54315715dad9173bb7b
oohyun15 <sakiss@ajou.ac.kr>
2020-10-25 13:16:57 +0900

    Fix minor bug

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "tonem0809@gmail.com"
+  # default from: "sakiss4774@gmail.com"
+
   layout 'mailer'
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,7 +63,7 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :sendgrid_actionmailer
   config.action_mailer.sendgrid_actionmailer_settings = {
-    api_key: ENV["SENDGRID_API"]
+    api_key: ENV["SENDGRID_API_KEY"]
   }
 
   config.middleware.use ExceptionNotification::Rack,
