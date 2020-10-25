407ddb3329e800f7ae2cec9ebc011182df3e2e57
MaSungHo <tonem123@naver.com>
2020-10-25 14:46:33 +0900

    Edit chat serializer

--- app/serializers/chat_serializer.rb
+++ app/serializers/chat_serializer.rb
@@ -1,13 +1,13 @@
 class ChatSerializer < ActiveModel::Serializer
   #has_many :messages  
-  attributes %i(chat_simple_info)
+  attributes %i(chat_info)
 
   #채팅방 목록들
-  def chat_simple_info
-    chat_simple_scope = ActiveModel::Type::Boolean.new.cast(scope.dig(:params, :chat_simple_info))
-    {id: object.id, created_time: object.messages.last.created_at, 
-      num_unchecked: object.messages.where.not("check_id @> ?", "{#{@instance_options[:user_id]}}").size, 
-      message: object.messages.last.body}
+  def chat_info
+    chat_scope = ActiveModel::Type::Boolean.new.cast(scope.dig(:params, :chat_info))
+    { id: object.id, post_id: object.post_id, created_time: object.has_message ? object.messages.last.created_at : nil, 
+      num_unchecked: object.has_message ? object.messages.where.not("check_id @> ?", "{#{@instance_options[:user_id]}}").size : nil, 
+      message: object.has_message ? object.messages.last.body : nil}
   end
 
 end
--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,3 +1,11 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes %i(message_info)
+
+  #채팅방 목록들
+  def message_info
+    message_scope = ActiveModel::Type::Boolean.new.cast(scope.dig(:params, :message_info))
+    {id: object.id, created_time: object.created_at, post_id: object.chat.post_id,
+      body: object.body, sender: object.user_id}
+  end
+  
 end
