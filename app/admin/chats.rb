409b906c43342d09921b422e9b3a088e4ac03dbf
oohyun15 <sakiss@ajou.ac.kr>
2020-10-26 18:58:15 +0900

    Add admin page: chat index

--- app/admin/chats.rb
+++ app/admin/chats.rb
@@ -1,19 +1,12 @@
 ActiveAdmin.register Chat do
   menu parent: "2. 서비스 관리", priority: 2, label: "#{I18n.t("activerecord.models.chat")} 관리"
+  filter :post, as: :select, multiple: true, collection: Post.all.map{|type| [type.title, type.id]}, label: "#{I18n.t("activerecord.models.post")} 필터"
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :post_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:post_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    id_column
+    column :post
+    column :user_list do |chat| chat.users end
+    column :message_count do |chat| chat.messages.size end
+    column :last_message do |chat| chat.has_message ? chat.messages.last&.body&.truncate(27) : "메시지가 없습니다." end
+  end
 end
