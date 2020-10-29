10307adb80b4c0604be55e788c4421d66abfc790
MaSungHo <tonem123@naver.com>
2020-10-29 22:35:20 +0900

    Edit like serializer

--- app/serializers/like_serializer.rb
+++ app/serializers/like_serializer.rb
@@ -3,10 +3,15 @@ class LikeSerializer < ActiveModel::Serializer
 
   def like_info
     user_simple_scope = ActiveModel::Type::Boolean.new.cast(scope.dig(:params, :like_info))
+    object.target_type == "User" ?
     {id: object.id, target_id: object.target_id, target_type: object.target_type, 
-      user_id: object.user_id, 
-      location: object.target_type == "User" ? User.find(object.target_id).location.title : nil,
-      name: object.target_type == "User" ? User.find(object.target_id).nickname : nil
+      user_id: object.user_id,
+      location: User.find(object.target_id).location.title,
+      name: User.find(object.target_id).nickname,
+      image_path: User.find(object.target_id).image_path
+    } : 
+    {
+      id: object.id, target_id: object.target_id, target_type: object.target_type, user_id: object.user_id
     }
   end
 
