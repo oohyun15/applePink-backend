610c7e98a0c17d08d22bf106b79076215df6b6d3
MaSungHo <tonem123@naver.com>
2020-10-29 20:58:39 +0900

    Edit serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,15 +1,10 @@
 class UserSerializer < ActiveModel::Serializer
   has_many :posts
-  attributes %i(user_simple_info user_detail_info)
+  attributes %i(user_info)
 
-  def user_simple_info
-    user_simple_scope = ActiveModel::Type::Boolean.new.cast(scope.dig(:params, :user_simple_info))
+  def user_info
+    user_simple_scope = ActiveModel::Type::Boolean.new.cast(scope.dig(:params, :user_info))
     {id: object.id, title: object.email, nickname: object.nickname, location_title: object.location&.title, expire_time: object.expire_time}
-  end
-
-  def user_detail_info
-    user_detail_scope = ActiveModel::Type::Boolean.new.cast(scope.dig(:params, :user_detail_info))
-    {id: object.id, title: object.email, nickname: object.nickname, location_title: object.location&.title, user_type: object.user_type} if user_detail_scope rescue nil
   end
 
 end
