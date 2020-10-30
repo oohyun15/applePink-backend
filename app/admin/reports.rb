1edc2440a16b2baa980f33199a0a6be77966384d
MaSungHo <tonem123@naver.com>
2020-10-30 16:44:28 +0900

    Add report admin page

---
+++ app/admin/reports.rb
@@ -0,0 +1,31 @@
+ActiveAdmin.register Report do
+  menu parent: "2. 서비스 관리", priority: 4, label: "#{I18n.t("activerecord.models.report")} 관리"
+  
+  filter :user, label: "#{I18n.t("activerecord.attributes.booking.user")} 필터"
+  filter :reason, as: :select, multiple: true, collection: Report.reasons.map{|type| [I18n.t("enum.report.reason.#{type[0]}"), type[1]]}, label: "#{I18n.t("activerecord.attributes.report.reason")} 필터"
+  
+  index do
+    selectable_column
+    id_column
+    br
+    column :image do |report| image_tag(report&.image_path ,class: 'admin-index-image') end
+    column :reason do |report| I18n.t("enum.report.reason.#{report.reason}") end
+    column :report_target_type do |report| report.report_target_type == "User" ? User.find(report.report_target_id).nickname : Post.find(report.report_target_id).title end
+    column :detail
+    column :created_at do |report| short_date report.created_at end
+    column :updated_at do |report| short_date report.updated_at end
+    actions
+  end
+
+  show do |report|
+    attributes_table do
+      row :image do |report| image_tag(report&.image_path ,class: 'admin-show-image') end
+      row :reason do |report| I18n.t("enum.report.reason.#{report.reason}") end
+      row :report_target_type do |report| report.report_target_type == "User" ? User.find(report.report_target_id).nickname : Post.find(report.report_target_id).title end
+      row :detail
+      row :created_at do |report| short_date report.created_at end
+      row :updated_at do |report| short_date report.updated_at end
+    end
+  end
+  
+end
