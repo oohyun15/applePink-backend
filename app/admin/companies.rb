b2aaccc61311bc7859129939dab37259037cabd6
oohyun15 <sakiss@ajou.ac.kr>
2020-11-01 16:44:39 +0900

    Add admin page:company

--- app/admin/companies.rb
+++ app/admin/companies.rb
@@ -20,5 +20,38 @@ ActiveAdmin.register Company do
         link_to "승인하기", confirm_company_path(company.id, approve: true), method: :patch
       end
     end
+    actions
+  end
+
+  show do
+    attributes_table do
+      row :user
+      row :name
+      row :image do |company| image_tag(company&.image_path ,class: 'admin-show-image') end
+      row :phone
+      row :location
+      row :message
+      row :description
+      tag_row :approve do |company| company.approve ? "accepted" : "rejected" end
+      row("승인관리") do |company|
+        if company.approve
+          link_to "승인취소", confirm_company_path(company.id, approve: false), method: :patch
+        else
+          link_to "승인하기", confirm_company_path(company.id, approve: true), method: :patch
+        end
+      end
+    end
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :phone
+      f.input :location
+      f.input :message
+      f.input :description
+      f.input :image, as: :file, hint: image_tag(f.object&.image_path, class: 'admin-show-image')
+      f.input :approve
+    end
   end
 end
