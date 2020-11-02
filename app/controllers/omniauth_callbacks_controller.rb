281a540e8edb03a2aee1a9182fb382151aeae993
oohyun15 <sakiss@ajou.ac.kr>
2020-11-02 17:04:31 +0900

    Fix minor bug

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -21,7 +21,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       # render json: { token: payload(@user), nickname: @user.nickname }, status: :ok
       @result = { token: payload(@user), id: @user.id }.to_json
-      render "shared/alert"
+      render "shared/login"
     else
       render json: { error: "로그인 에러가 발생하였습니다." }, status: :not_found
     end
