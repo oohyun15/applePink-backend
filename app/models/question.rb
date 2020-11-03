e46c3b94d94074d8173e1e567e72c08dc5a139a4
MaSungHo <tonem123@naver.com>
2020-11-03 18:07:31 +0900

    Add create action

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,7 +1,11 @@
 class Question < ApplicationRecord
   include Imagable
 
-  QUESTION_COLUMNS = %i(title body contact email) + [images_attributes: %i(imagable_type imagable_id image)]
+  QUESTION_COLUMNS = %i(title body contact) + [images_attributes: %i(imagable_type imagable_id image)]
+
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :contact, presence: true
 
   belongs_to :user
 end
