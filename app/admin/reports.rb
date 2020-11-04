1124950c6d511a397d253d77dffbd93ebb8c5b0d
Yonghyun Kim <sakiss@ajou.ac.kr>
2020-11-04 22:15:09 +0900

    Update reports.rb

--- app/admin/reports.rb
+++ app/admin/reports.rb
@@ -9,18 +9,22 @@ ActiveAdmin.register Report do
     selectable_column
     id_column
     br
+    column :user
+    #  column :target_type do |report| report.target_type == "User" ? User.find(report.target_id).nickname : Post.find(report.target_id).title end
+    column :target
     column :reason do |report| I18n.t("enum.report.reason.#{report.reason}") end
-    column :target_type do |report| report.target_type == "User" ? User.find(report.target_id).nickname : Post.find(report.target_id).title end
     column :detail do |report| report.detail&.truncate(20) end
     column :created_at do |report| short_date report.created_at end
     column :updated_at do |report| short_date report.updated_at end
     actions
   end
 
-  show do |report|
+  show do
     attributes_table do
+      row :user
+      row :target
       row :reason do |report| I18n.t("enum.report.reason.#{report.reason}") end
-      row :target_type do |report| report.target_type == "User" ? User.find(report.target_id).nickname : Post.find(report.target_id).title end
+      #  row :target_type do |report| report.target_type == "User" ? User.find(report.target_id).nickname : Post.find(report.target_id).title end
       row :detail
       row :created_at do |report| short_date report.created_at end
       row :updated_at do |report| short_date report.updated_at end
