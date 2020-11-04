a9d15c2daf731642dcd2538ce53723324bad2196
MaSungHo <tonem123@naver.com>
2020-11-04 19:24:52 +0900

    Fix minor bug

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -1,3 +1,12 @@
 require 'rails_helper'
 
+describe "Post test", type: :request do
+  before(:all) do
+    @id = User.first.id
+    @email = User.first.email
+
+    post "/users/sign_in", params: {user: {email: "#{@email}", password: "test123"}}
+    @token =  JSON.parse(response.body)["token"]
+  end
+
 end
--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -19,19 +19,33 @@ describe "User test", type: :request do
 
   #앞선 테스트에서 생성한 유저의 정보를 수정하는 테스트
   it 'user update test' do
-    u = User.find_by(email: "tonem123@naver.com")
-    put "/users/#{u.id}", params: {user: {email: "change123@naver.com", nickname: "change123", 
+    user_before = User.find_by(email: "tonem123@naver.com")
+    put "/users/#{user_before.id}", params: {user: {email: "change123@naver.com", nickname: "change123", 
       password: "change123", password_confirmation: "change123"}}, headers: {Authorization: @token}
 
     #이전 user id로 객체의 정보가 업데이트 되었는지 확인함.
-    expect(User.find(u.id).email).to eq("change123@naver.com")
+    user_after = User.find(user_before.id)
+    expect(user_after.email).to eq("change123@naver.com")
+    expect(user_after.nickname).to eq("change123")
+  end
+
+  it 'user range test' do
+    user_before = User.find_by(email: "change123@naver.com")
+    post "/users/sign_in", params: {user: {email: user_before.email, password: "change123"}}
+
+    before_range = user_before.location_range
+    put range_users_path, params: {user: {location_range: "location_far"}}, headers: {Authorization: JSON.parse(response.body)["token"]}
+
+    user_after = User.find(user_before.id)
+    expect(before_range.eql? user_after.location_range).to eq(false)
+    expect(user_after.location_range).to eq("location_far")
   end
 
   it 'user withdrawal test' do
-    u = User.find_by(email: "change123@naver.com")
-    post "/users/sign_in", params: {user: {email: u.email, password: "change123"}} 
+    user = User.find_by(email: "change123@naver.com")
+    post "/users/sign_in", params: {user: {email: user.email, password: "change123"}} 
     delete '/users/withdrawal', headers: {Authorization: JSON.parse(response.body)["token"]}
-    expect(User.where(id: u.id).present?).to eq(false)
+    expect(User.where(id: user.id).present?).to eq(false)
   end
 
   #전체 유저 목록을 불러오는지 확인
@@ -59,20 +73,14 @@ describe "User test", type: :request do
   end
 
   it 'user list test' do
-    get list_user_path id: @id, params: {post_type: "provide"}, headers: {Authorization: @token}
-    posts = JSON.parse(response.body)["posts"]
+    get list_user_path(@id), params: {post_type: "provide"}, headers: {Authorization: @token}
 
-    #결과로 나온 post들의 post_type이 모두 "provide인지 확인"
-    is_provide = true
+    posts = JSON.parse(response.body)
     
-    #post들이 나오지 않을 땐 바로 테스트 통과시키기
-    unless posts == nil
-      for i in 0..(posts.length - 1)
-        if posts[i]["post_type"] != "provide"
-          is_provide = false
-        end
-      end
-    end
-    expect(is_provide).to eq(true)
+    result = Post.where(["user_id = :user_id and post_type = :post_type", { user_id: @id, post_type: 0 }])
+    
+    #현재는 길이 비교만 하는 중. 
+    expect(posts.length).to eq(result.length)
   end
+  
 end
