8c035c1e1ffbe360ae0ccc19216f05b52b9249cd
MaSungHo <tonem123@naver.com>
2020-11-05 20:58:39 +0900

    Fix minor bug

--- app/admin/reports.rb
+++ app/admin/reports.rb
@@ -28,21 +28,14 @@ ActiveAdmin.register Report do
       row :detail
       row :created_at do |report| short_date report.created_at end
       row :updated_at do |report| short_date report.updated_at end
-    end
-    panel '이미지 목록' do
-      table_for collection do
-          column "이미지" do |report|
-            report.images.exists? ? (
-              table_for "이미지" do
-                report.images.each_with_index do |image, index|
-                  column "첨부 이미지 #{index + 1}" do
-                    image_tag(image.image_path ,class: 'admin-index-image')
-                  end
-                end
-              end
-            )
-            : "없음"
+      panel '이미지 리스트' do
+        table_for '이미지' do
+          report.images.each_with_index do |image, index|
+            column "상세이미지#{index + 1}" do
+              image_tag(image.image_path ,class: 'admin-show-image')
+            end
           end
+        end
       end
     end
   end
