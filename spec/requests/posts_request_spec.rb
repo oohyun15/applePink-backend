e58c0ecb031c280233720bdf59e633f13668a795
MaSungHo <tonem123@naver.com>
2020-11-09 20:26:19 +0900

    Add post action tests

--- spec/requests/posts_request_spec.rb
+++ spec/requests/posts_request_spec.rb
@@ -3,8 +3,9 @@ require 'active_support'
 
 describe "Post test", type: :request do
   before(:all) do
-    @id = User.last.id
-    @email = User.last.email
+    # 유저 중 임의의 유저를 선택함.
+    @id = User.all.ids.sample
+    @email = User.find(@id).email
 
     post "/users/sign_in", params: {user: {email: "#{@email}", password: "test123"}}
     @token =  JSON.parse(response.body)["token"]
@@ -16,14 +17,18 @@ describe "Post test", type: :request do
     get "/posts", params: {location_title: "삼성동"}, headers: {Authorization: @token}
     expect(response).to have_http_status(404)
 
-    # response로 넘어온 post들이 "파장동" post인지 확인
+    # response로 넘어온 post들이 랜덤으로 선택된 지역의 post인지 확인
     # 길이가 같은 것으로 확인
-    get "/posts", params: {location_title: "파장동"}, headers: {Authorization: @token}
-    unless JSON.parse(response.body).empty?
-      posts = JSON.parse(response.body).select { |post| post["post_info"]["location"] == "파장동" }
-      expect(posts.length).to eq(JSON.parse(response.body).length)
+    title = Location.all.pluck(:title).sample
+    get "/posts", params: {location_title: title}, headers: {Authorization: @token}
+
+    posts = Post.where(title: title).ids
+    ids = []
+    JSON.parse(response.body).each do |post| 
+      ids << post["post_info"]["id"]
     end
-    
+    expect(posts).to eq(ids)
+
     # 사용자 거리 설정에 따라 index로 나오는 post의 결과가 달라짐.
     user = User.find(@id)
     location_positions = []
@@ -58,6 +63,65 @@ describe "Post test", type: :request do
       ids << post["post_info"]["id"]
     end
     expect(posts).to eq(ids)
+  end
 
+  it "post show test" do
+    post_id = Post.first.id
+    
+    get "/posts/#{post_id}", headers: {Authorization: @token}
+    expect(JSON.parse(response.body)["post_info"]["id"]).to eq(post_id)
+  end
+
+  it "post create test" do
+    # 생성에 필요한 정보가 없을 때 bad request 상태가 뜸
+    post_info = { post: {title: nil, body: "2일부터 8일까지 대여해줍니다.", price: 21000,
+      post_type: :provide, category_id: 4} }
+      post "/posts", params: post_info, headers: {Authorization: @token}
+    expect(response).to have_http_status(400)
+
+    #생성할 post의 정보를 정의함
+    post_info = { post: {title: "맥북 15인치 대여합니다.", body: "2일부터 8일까지 대여해줍니다.", price: 21000,
+      post_type: :provide, category_id: 4} }
+    
+    post "/posts", params: post_info, headers: {Authorization: @token}
+    expect(response).to have_http_status(200)
+  end
+
+  it "post update test" do
+    post = Post.last
+
+    update_info = { post: {title: "업데이트", body: "업데이트되어야 함", price: 10000,
+      post_type: "ask", category_id: 2} }
+    
+    # 위의 내용대로 post를 update 함.
+    put "/posts/#{post.id}", params: update_info, headers: {Authorization: @token}
+
+    post = Post.last
+    expect(post.title).to eq(update_info[:post][:title])
+    expect(post.body).to eq(update_info[:post][:body])
+    expect(post.price).to eq(update_info[:post][:price])
+    expect(post.post_type).to eq(update_info[:post][:post_type])
+    expect(post.category_id).to eq(update_info[:post][:category_id])
+  end
+
+  it "post delete test" do
+    # last id가 위 테스트에서 생성한 post의 id임.
+    post_id = Post.last.id
+
+    delete "/posts/#{post_id}", headers: {Authorization: @token}
+    expect(response).to have_http_status(200)
+  end
+
+  it "post like test" do
+    ids = Post.all.ids
+    id = ids.sample
+
+    # post에 좋아요한 유저의 이름을 반환함
+    get "/posts/#{id}/like", headers: {Authorization: @token}
+    user_list = JSON.parse(response.body)["user_list"]
+    
+    # 데이터베이스에서 직접 찾은 내용과 일치하는지 확인
+    user_nicknames = Post.find(id).likes.pluck(:user_id)
+    expect(user_list).to eq(user_nicknames)
   end
 end
