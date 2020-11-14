788abf8d8076dca47477d9089740689cf61154d9
oohyun15 <sakiss@ajou.ac.kr>
2020-11-14 19:45:37 +0900

    Fix minor bug

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,7 +6,8 @@ class MessagesController < ApplicationController
   def index
     # 현재 유저가 읽지 않은 메시지 가져오기
     @messages = @chat.messages.where.not("check_id @> ?", "{#{current_user.id}}")
-    
+    @messages.order(created_at: :asc)
+
     # 메시지에 현재 유저가 읽었다고 추가
     @messages.each do |message|
       message.check_id << current_user.id
@@ -34,7 +35,7 @@ class MessagesController < ApplicationController
 
     Rails.logger.info "From #{current_user.nickname} To #{users}"
     Rails.logger.info "registration ids: #{rids}"
-    push_notification("#{current_user.nickname} : #{@message.body}", "[모두나눔] #{@chat.post.title.truncate(10)}", rids)
+    push_notification("#{current_user.nickname} : #{@message.body}", "", rids)
 
     render json: @message, status: :ok, scope: {params: create_params}
   end
