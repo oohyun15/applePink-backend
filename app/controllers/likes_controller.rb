7d1a82b258ad9d78ec6832f7378ed703683e73a8
oohyun15 <sakiss@ajou.ac.kr>
2020-11-14 20:04:54 +0900

    Fix minor bug
    - 좋아요 로그 수정

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -16,8 +16,10 @@ class LikesController < ApplicationController
 
   def toggle
     # 좋아요 대상이 자신인 경우
-    Rails.logger.error "ERROR: 자기 자신은 좋아요를 할 수 없습니다. #{log_info}"
-    return render json: {error: "자기 자신은 좋아요를 할 수 없습니다."}, status: :bad_request if (@target.user rescue @target) == current_user 
+    if (@target.user rescue @target) == current_user 
+      Rails.logger.error "ERROR: 자기 자신은 좋아요를 할 수 없습니다. #{log_info}"
+      return render json: {error: "자기 자신은 좋아요를 할 수 없습니다."}, status: :bad_request 
+    end
 
     begin
       # 기존에 좋아요를 했을 경우
