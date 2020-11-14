39c117a02753790b362a0ad6989ce1e8d9b82091
MaSungHo <tonem123@naver.com>
2020-11-14 18:54:36 +0900

    Add date columns to bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_155013) do
+ActiveRecord::Schema.define(version: 2020_11_14_095142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -75,6 +75,8 @@ ActiveRecord::Schema.define(version: 2020_11_13_155013) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "lent_day"
     t.text "contract"
+    t.datetime "provider_sign_datetime"
+    t.datetime "consumer_sign_datetime"
     t.index ["post_id"], name: "index_bookings_on_post_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
