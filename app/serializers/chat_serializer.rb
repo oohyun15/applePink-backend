6db35a2fc45b6f794081b426fceffafeac67849d
oohyun15 <sakiss@ajou.ac.kr>
2020-11-16 03:24:35 +0900

    Fix minor bug
    - 마지막 채팅 시간 수정

--- app/serializers/chat_serializer.rb
+++ app/serializers/chat_serializer.rb
@@ -9,7 +9,7 @@ class ChatSerializer < ActiveModel::Serializer
       id: object.id,
       post_id: object.post_id,
       nickname: User.where(id: (object.user_ids - [@instance_options[:user_id]]) )&.pluck(:nickname),
-      created_time: object.messages_count > 0 ? object.messages.last.created_at : nil, 
+      created_time: object.messages_count > 0 ? time_ago_in_words(object.messages.last.created_at) : nil, 
       num_unchecked: object.messages_count > 0 ? object.messages.where.not("check_id @> ?", "{#{@instance_options[:user_id]}}").size : nil, 
       message: object.messages_count > 0 ? object.messages.last.body : nil
     }
