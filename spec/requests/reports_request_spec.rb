2b195f699c00538863be84c4d5091bebe1b2a9f6
MaSungHo <tonem123@naver.com>
2020-11-16 22:15:51 +0900

    Create report request test

--- spec/requests/reports_request_spec.rb
+++ spec/requests/reports_request_spec.rb
@@ -51,5 +51,42 @@ describe "Report test", type: :request do
     end
     report_ids = Report.where(user_id: @id).ids
     expect(report_ids - ids).to eq([])
+
+    Report.delete_all
+  end
+
+  it "report create test" do
+    # 자기 자신을 신고하는 경우 에러 발생
+    report_info = {
+      report: {
+        target_id: @id,
+        target_type: "user",
+        detail: "테스트 신고",
+        reason: Faker::Number.between(from: 0, to: 7)
+      }
+    }
+    post "/reports", params: report_info, headers: {Authorization: @token}
+    expect(response).to have_http_status(:bad_request)
+    
+    # 자기 자신이 아닌 대상을 신고함.
+    target_type = Report::REPORT_MODELS.sample
+    model = target_type.capitalize
+    target_id = (model == "User") ? (model.constantize.all.ids - [@id]).sample : model.constantize.all.ids.sample
+    report_info = {
+      report: { 
+        target_id: target_id,
+        target_type: target_type,
+        detail: "테스트 신고",
+        reason: "fraud"
+      }
+    }
+    post "/reports", params: report_info, headers: {Authorization: @token}
+    expect(response).to have_http_status(:ok)
+    
+    # 이미 신고한 대상을 다시 신고할 때 오류가 발생함.
+    post "/reports", params: report_info, headers: {Authorization: @token}
+    expect(response).to have_http_status(:bad_request)
+
+    Report.first.delete
   end
 end
