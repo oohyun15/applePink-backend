da79c9236e50fb80a190fef11b9e5728f032cb9b
oohyun15 <sakiss@ajou.ac.kr>
2020-11-17 22:00:31 +0900

    Fix minor bug

--- app/controllers/kakaocert_controller.rb
+++ app/controllers/kakaocert_controller.rb
@@ -101,7 +101,8 @@ class KakaocertController < ApplicationController
       )
       # 전자서명이 완료되었을 때
       if @response["state"] == 1
-        return redirect_to kakaocert_verifyESign_path(receiptId: params[:receiptId], booking_id: params[:booking_id]), headers: {Authorization: @token}
+        # return redirect_to kakaocert_verifyESign_path(receiptId: params[:receiptId], booking_id: params[:booking_id]), headers: {Authorization: @token}
+        return render json: @response
       # 전자서명이 미완료되었을 때
       elsif @response["state"] == 0
         Rails.logger.error "전자서명이 완료되지 않았습니다. #{log_info}" 
