ca73ad69480c5fe0b22671d3e4c447ff2479f446
MaSungHo <tonem123@naver.com>
2020-11-18 17:30:51 +0900

    Add tests

--- spec/requests/companies_request_spec.rb
+++ spec/requests/companies_request_spec.rb
@@ -1,5 +1,35 @@
 require 'rails_helper'
+require 'active_support'
 
-RSpec.describe "Companies", type: :request do
+describe "Company test", type: :request do
+  before(:all) do
+    # 유저 중 임의의 유저를 선택함.
+    @user = User.all.sample
+    @id = @user.id
+    @email = @user.email
 
+    post "/users/sign_in", params: {user: {email: "#{@email}", password: "test123"}}
+    @token =  JSON.parse(response.body)["token"]
+  end
+
+  it "Company create test" do
+    company_info = {
+      company: {
+        name: ,
+        phone: ,
+        message: ,
+        description: ,
+        image: File.open("#{Rails.root}/public/image/default.png"),
+        location_id: ,
+      }
+    }
+  end
+
+  it "Company destroy test" do
+    
+  end
+
+  it "Company confirm test" do
+    
+  end
 end
