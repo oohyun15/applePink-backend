6400b6249589fa86a450403d41b1713010584e45
oohyun15 <sakiss@ajou.ac.kr>
2020-11-23 21:05:18 +0900

    Edit message controller
    - Firebase로 채팅 메시지 관리 -> response 안줌

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -15,7 +15,7 @@ class MessagesController < ApplicationController
     end
 
     # 메시지 렌더
-    render json: @messages, status: :ok, scope: {params: create_params}
+    # render json: @messages, status: :ok, scope: {params: create_params}
   end
 
   def create
@@ -37,7 +37,7 @@ class MessagesController < ApplicationController
     Rails.logger.error "registration ids: #{rids}"
     push_notification("#{current_user.nickname} : #{@message.body}", "", rids)
 
-    render json: @message, status: :ok, scope: {params: create_params}
+    # render json: @message, status: :ok, scope: {params: create_params}
   end
 
   private
