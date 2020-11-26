639bdabc9c0d115d7ea86d2146093055c564388c
MaSungHo <tonem123@naver.com>
2020-11-26 20:04:14 +0900

    Create review test

--- spec/requests/reviews_request_spec.rb
+++ spec/requests/reviews_request_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
+require 'active_support'
 
-RSpec.describe "Reviews", type: :request do
+describe "Review test", type: :request do
+  before(:all) do
+    # 유저 중 임의의 유저를 선택함.
+    @user = User.all.sample
+    @id = @user.id
+    @email = @user.email
 
+    post "/users/sign_in", params: {user: {email: "#{@email}", password: "test123"}}
+    @token =  JSON.parse(response.body)["token"]
+  end
+
+  it "review index test" do
+    
+  end
 end
