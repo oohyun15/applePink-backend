b3899fdb237a55c02d5a8ab6cfaf2684848f3ebc
MaSungHo <tonem123@naver.com>
2020-11-28 16:32:16 +0900

    Fix minor bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :load_user, only: %i(show update list)
-  before_action :authenticate_user!, except: %i(create)
+  before_action :authenticate_user!, except: %i(create sms_auth)
   before_action :check_email, only: %i(email_auth)
 
   # 유저 목록 보기
