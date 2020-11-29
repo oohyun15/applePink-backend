dc2ee7fe223786513d495cb279a5058a74a3d5b9
MaSungHo <tonem123@naver.com>
2020-11-29 15:51:06 +0900

    Edit user mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,12 +2,21 @@ class UserMailer < ApplicationMailer
   default from: "tonem0809@gmail.com"
   # default from: "sakiss4774@gmail.com"
 
-  def generate_code(email, generated_code)
+  def generate_code(email, generated_code, type)
     @email = email
     @generated_code = generated_code
-    mail(
-      to: email,
-      subject: "[모두나눔] 소속 인증을 위해 확인해주세요!"
-    )
+    @type = type
+    case @type
+    when "group"
+      mail(
+        to: email,
+        subject: "[모두나눔] 소속 인증을 위해 확인해주세요!"
+      )
+    when "find"
+      mail(
+        to: email,
+        subject: "[모두나눔] 비밀번호 찾기를 위한 인증 번호입니다."
+      )
+    end
   end
 end
