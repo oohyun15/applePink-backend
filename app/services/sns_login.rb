08b752e57a9e98811a5fe6c81592c5460f9fc6c6
oohyun15 <sakiss@ajou.ac.kr>
2020-11-30 04:34:01 +0900

    Fix minor bug

--- app/services/sns_login.rb
+++ app/services/sns_login.rb
@@ -13,8 +13,9 @@ class SnsLogin
       begin
         user = User.new get_auth_params
         #user.remote_image_url = @auth.info.image
-        user.remote_image_url = @auth.extra.properties.profile_image if @auth.extra&.properties&.profile_image&.present?
         Rails.logger.error "OAUTH USER:\n#{user}"
+        Rails.logger.error "AUTH:\n#{@auth}"
+        user.remote_image_url = @auth.extra&.properties&.profile_image if @auth.extra&.properties&.profile_image&.present?
         user.save!
       rescue => e
         Rails.logger.error "ERROR: #{e}"
