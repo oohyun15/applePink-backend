620f9e063bf9584ceef03b24c2943d74a9e2940c
oohyun15 <sakiss@ajou.ac.kr>
2020-12-08 03:40:53 +0900

    Add term of use
    - 이용약관 추가

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,3 +11,14 @@
 // about supported directives.
 //
 //= require rails-ujs
+
+function simpleFormat(str) {
+  str = str.replace(/\r\n?/, "\n");
+  str = $.trim(str);
+  if (str.length > 0) {
+    str = str.replace(/\n\n+/g, '</p><p>');
+    str = str.replace(/\n/g, '<br />');
+    str = '<p>' + str + '</p>';
+  }
+  return str;
+}
