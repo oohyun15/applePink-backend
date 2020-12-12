8a7776b202833e09dfea8ba5301403a282703072
MaSungHo <tonem123@naver.com>
2020-12-12 17:20:46 +0900

    Fix minor bug

--- app/admin/reviews.rb
+++ app/admin/reviews.rb
@@ -1,9 +1,9 @@
 ActiveAdmin.register Review do
   menu parent: "2. 서비스 관리", priority: 4, label: "#{I18n.t("activerecord.models.review")} 관리"
   
-  filter :user, label: "#{I18n.t("activerecord.attributes.review.user_id")} 필터"
-  filter :post, label: "#{I18n.t("activerecord.attributes.review.post_id")} 필터"
-  filter :booking, label: "#{I18n.t("activerecord.attributes.review.booking_id")} 필터"
+  filter :user, label: "#{I18n.t("activerecord.attributes.review.user")} 필터"
+  filter :post, label: "#{I18n.t("activerecord.attributes.review.post")} 필터"
+  filter :booking, label: "#{I18n.t("activerecord.attributes.review.booking")} 필터"
   filter :body_cont, label: "#{I18n.t("activerecord.attributes.review.body")} 필터"
 
   index do
@@ -12,7 +12,7 @@ ActiveAdmin.register Review do
     br
     column :user
     column :post
-    column :booking_id do |review| review.booking end
+    column :booking do |review| review.booking end
     column :rating
     column :body do |review| review.body&.truncate(20) end
     column :created_at do |review| short_date review.created_at end
@@ -24,7 +24,7 @@ ActiveAdmin.register Review do
     attributes_table do
       row :user
       row :post
-      row :booking_id do |review| review.booking end
+      row :booking do |review| review.booking end
       row :rating
       row :body
       row :created_at do |report| short_date report.created_at end
