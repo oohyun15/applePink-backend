a268dd94452bc9dbcb3ce74b8ca6790e3b024da0
oohyun15 <sakiss@ajou.ac.kr>
2020-12-14 00:32:39 +0900

    Fix minor bug

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,10 @@
 class Review < ApplicationRecord
   include Imagable
+  
   REVIEW_COLUMNS = %i(body rating booking_id) + [images_attributes: %i(imagable_type imagable_id image)]
+
+  validates :rating, presence: true
+  validates :body, presence: true
 
   has_many :reports, class_name: "Report", as: :target, dependent: :destroy
 
