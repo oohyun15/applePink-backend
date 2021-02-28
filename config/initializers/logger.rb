1d798903a8e0116928fd9274ac13769ff47b8641
oohyun15 <sakiss4774@gmail.com>
2021-02-28 19:54:58 +0900

    Fix minor bug

--- config/initializers/logger.rb
+++ config/initializers/logger.rb
@@ -1,7 +1,7 @@
 class ActionDispatch::DebugExceptions
   alias_method :old_log_error, :log_error
   def log_error(env, wrapper)
-    if Rails.env.production? && wrapper.exception.is_a? ActionController::RoutingError
+    if Rails.env.production? && wrapper.exception.is_a?(ActionController::RoutingError)
       return
     else
       old_log_error env, wrapper
